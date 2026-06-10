76109a94bcab974f169a2b668aaa084a146f5569
anhpt135 <84607090+anhpt135@users.noreply.github.com>
2026-06-10 08:28:02 +0700

    40_Tao_Interface_CleanupAfterDeath

---
+++ Combat/Source/Combat/Private/Interfaces/CleanupAfterDeathInterface.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Interfaces/CleanupAfterDeathInterface.h"
+
+// Add default functionality here for any ICleanupAfterDeathInterface functions that are not pure virtual.
