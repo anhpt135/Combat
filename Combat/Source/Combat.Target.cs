f2f8ac215a203c837a00cbb69488966a47129bf6
anhpt135 <84607090+anhpt135@users.noreply.github.com>
2026-07-01 13:54:45 +0700

    50_Migrate_UIMaterialLab

--- Combat/Source/Combat.Target.cs
+++ Combat/Source/Combat.Target.cs
@@ -8,7 +8,7 @@ public class CombatTarget : TargetRules
 	public CombatTarget(TargetInfo Target) : base(Target)
 	{
 		Type = TargetType.Game;
-		DefaultBuildSettings = BuildSettingsVersion.V6;
+		DefaultBuildSettings = BuildSettingsVersion.Latest;
 
 		ExtraModuleNames.AddRange( new string[] { "Combat" } );
 	}
